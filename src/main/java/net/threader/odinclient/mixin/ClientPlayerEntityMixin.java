0219ee0813beb5aef97a3ace9113d952e6b7ede7
threader <40972803+localthreader@users.noreply.github.com>
2021-06-07 17:41:15 -0300

    Mixing ClientPlayerEntity for pre-processing commands instead of mixing ChatMessageC2SPacket

--- src/main/java/net/threader/odinclient/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/threader/odinclient/mixin/ClientPlayerEntityMixin.java
@@ -1,6 +1,6 @@
 package net.threader.odinclient.mixin;
 
-import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.threader.odinclient.OdinClient;
 import net.threader.odinclient.event.CommandProcessEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,14 +8,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatMessageC2SPacket.class)
-public class ChatMessageC2SPacketMixin {
-    @Inject(method = "<init>(Ljava/lang/String;)V", at = @At("HEAD"), cancellable = true)
-    public void onConstruct(String message, CallbackInfo ci) {
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+    public void sendChatMessage(String message, CallbackInfo ci) {
         if(message.startsWith(OdinClient.Metrics.getOdinCommandPrefix())) {
             CommandProcessEvent event = new CommandProcessEvent(message);
             OdinClient.INSTANCE.getEventProcessor().post(event);
             ci.cancel();
         }
     }
+
 }
