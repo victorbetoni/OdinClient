02c8d762ac82e60e19b161fce8f056b55d9d88c0
threader <40972803+localthreader@users.noreply.github.com>
2021-06-12 21:38:30 -0300

    Fixing redirector for markClosed method

--- src/main/java/net/threader/odinclient/mixin/RebuildTaskMixin.java
+++ src/main/java/net/threader/odinclient/mixin/RebuildTaskMixin.java
@@ -1,6 +1,5 @@
 package net.threader.odinclient.mixin;
 
-import net.fabricmc.fabric.mixin.client.indigo.renderer.MixinChunkRebuildTask;
 import net.minecraft.block.Block;
 import net.minecraft.client.render.chunk.ChunkBuilder;
 import net.minecraft.client.render.chunk.ChunkOcclusionDataBuilder;
@@ -20,17 +19,16 @@ import java.util.Objects;
 public class RebuildTaskMixin {
     @Shadow @Nullable protected ChunkRendererRegion region;
 
-    @Redirect(method = "render", remap = false, at = @At(value = "HEAD", target = "Lnet/minecraft/client/render/chunk;markClosed(Lnet/minecraft/util/math/BlockPos;)V"))
-    public void render(BlockPos pos) {
+    @Redirect(method = "render", remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/ChunkOcclusionDataBuilder;markClosed(Lnet/minecraft/util/math/BlockPos;)V"))
+    public void render(ChunkOcclusionDataBuilder chunkOcclusionDataBuilder, BlockPos pos) {
         ChunkRendererRegion region = this.region;
         if(region != null) {
             Block block = Objects.requireNonNull(region.getBlockEntity(pos)).getCachedState().getBlock();
             BlockTraluscenscyDefineEvent event = new BlockTraluscenscyDefineEvent(block);
             OdinClient.INSTANCE.getEventProcessor().post(event);
             if(!event.isCanceled()) {
-                ChunkOcclusionDataBuilder builder = (ChunkOcclusionDataBuilder) (Object) this;
-                builder.closed.set(ChunkOcclusionDataBuilder.pack(pos), true);
-                --builder.openCount;
+                chunkOcclusionDataBuilder.closed.set(ChunkOcclusionDataBuilder.pack(pos), true);
+                --chunkOcclusionDataBuilder.openCount;
             }
         }
     }
